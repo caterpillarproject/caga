137830334feda5a7a61c4bdf624dc9d9a3c3e1bd
Alexander Ji <alexji@mit.edu>
2018-07-31 14:47:03 -0400

    Added some more basics, and a separate plotting module

--- caga/caga.py
+++ caga/caga.py
@@ -5,21 +5,46 @@ Modules for using Caterpillar-GAMMA data
 """
 
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
 
 from NuPyCEE import omega, sygma
 from JINAPyCEE import gamma, omega_plus
 
-def load_gamma_tree():
-    """
-    """
-    return gt
+## hardcode some index labels for GAMMA tree data
+all_ixs = np.arange(11)
+ix_halo_ID, ix_age, ix_z, ix_t_merge, ix_ID_merge, ix_m_halo, ix_r_vir, \
+    ix_is_prim, ix_redshifts, ix_times, ix_tree_trunk_ID = all_ixs
 
-def get_root_mass(g):
+def load_gamma_tree(path):
+    """ Load a gamma tree (from a .npy save) """
+    gtree = np.load(path, encoding='latin1')
+    assert len(all_ixs) == len(gtree)
+    return gtree
+
+def get_root_Mstar(gam):
     """
     """
-def get_root_metallicity(g):
+def get_root_ZH(gam):
     """
     """
-def get_root_Mpeak(g):
+def get_root_FeH(gam):
     """
     """
+def get_root_FeH_distr(gam):
+    """
+    """
+def get_root_Mpeak(gam):
+    """
+    """
+
+def precompute_ssps():
+    """
+    Pre-calculate the ejecta of simple stellar populations.
+    You can ignore the warning.
+    """
+    o_for_SSPs = omega.omega(special_timesteps=2, pre_calculate_SSPs=True)
+    # Copy the SSPs array
+    SSPs_in = [o_for_SSPs.ej_SSP, o_for_SSPs.ej_SSP_coef, o_for_SSPs.dt_ssp, o_for_SSPs.t_ssp]
+    return SSPs_in
+    
---
+++ caga/plot.py
@@ -0,0 +1,58 @@
+from __future__ import absolute_import, division
+
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+__all__ = ["mass_history"]
+
+def mass_history(gamtree):
+    """
+    Plot halo mass of a gamma tree, colored by branch
+    """
+    br_halo_ID, br_age, br_z, br_t_merge, br_ID_merge, \
+    br_m_halo, br_r_vir, br_is_prim, redshifts, times, \
+        tree_trunk_ID = gamtree
+    nb_redshifts = len(redshifts)
+    
+    # Set the virial mass threshold above which halos are allowed to form stars.
+    # Alternatively, this could be provided by the hydrodynamic simulation
+    m_vir_thresh = 3.0e6
+
+    # Fill the input array that will inform GAMMA whether or not a given branch
+    # will for stars
+    br_is_SF = []
+    for i_z in range(len(br_m_halo)):
+        br_is_SF.append([])
+        for i_b in range(len(br_m_halo[i_z])):
+            if max(br_m_halo[i_z][i_b]) >= m_vir_thresh:
+                br_is_SF[i_z].append(True)
+            else:
+                br_is_SF[i_z].append(False)
+    
+    # Set figure frame and font properties
+    fig = plt.figure(figsize=(6.5,4.5))
+    matplotlib.rcParams.update({'font.size': 13})
+
+    # Plot the evolution of the virial mass of each branch
+    # Non star-forming branches will be shown as thin gray lines
+    for i_z in range(nb_redshifts):
+        for i_b in range(len(br_halo_ID[i_z])):
+            if br_is_SF[i_z][i_b]:
+                plt.plot(br_z[i_z][i_b], br_m_halo[i_z][i_b], '.')
+            else:
+                plt.plot(br_z[i_z][i_b], br_m_halo[i_z][i_b], '.', color='0.7', linewidth=0.7)
+    plt.plot(redshifts, [m_vir_thresh]*nb_redshifts, ':k', label='Mass threshold')
+
+    # Set labels
+    plt.xlabel('Redshift', fontsize=14)
+    plt.ylabel('M$_\mathrm{vir}$ [M$_\odot$]', fontsize=14)
+    plt.legend(fontsize=13, frameon=False)
+    plt.yscale('log')
+
+    # Adjust the figure
+    plt.subplots_adjust(top=0.95)
+    plt.subplots_adjust(right=0.95)
+    plt.subplots_adjust(left=0.12)
+    plt.subplots_adjust(bottom=0.15)
+    return fig
