d7835aa754c54dd9e8ef9ae023e8c36335c20ca0
Alexander Ji <alexji@mit.edu>
2018-08-01 12:27:37 -0400

    Added some plotting tools and computing

--- caga/caga.py
+++ caga/caga.py
@@ -7,10 +7,43 @@ Modules for using Caterpillar-GAMMA data
 import numpy as np
 import itertools
 
-from NuPyCEE import omega, sygma
+from NuPyCEE import omega, sygma, stellab
 from JINAPyCEE import gamma, omega_plus
 
 from . import plot
+
+
+#### Define some element information
+# Use stellab to get solar values
+_stellab = stellab.stellab()
+_asp09 = _stellab.sol_norm[_stellab.paths_norm.index("Asplund_et_al_2009")]
+el_to_solar = dict([_elemnorm for _elemnorm in _asp09])
+# Mass number of the most abundant isotopes
+el_norm = ['H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne', 'Na', 'Mg',
+           'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr',
+           'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br',
+           'Kr', 'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Ru', 'Rh', 'Pd', 'Ag',
+           'Cd', 'In', 'Sn', 'Sb', 'Te', 'I', 'Xe', 'Cs', 'Ba', 'La', 'Ce',
+           'Pr', 'Nd', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb',
+           'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl',
+           'Pb', 'Bi', 'Th', 'U']
+mn_norm = [1.0000200000000001, 3.999834, 6.924099999999999, 9.0, 10.801,
+           12.011061999999999, 14.00229, 16.004379000000004, 19.0, 20.13891,
+           23.0, 24.3202, 27.0, 28.108575891424106, 31.0, 32.094199999999994,
+           35.4844, 36.30859999999999, 39.13588999999999, 40.115629999999996,
+           45.0, 47.9183, 50.9975, 52.05541, 55.0, 55.90993, 59.0, 58.759575,
+           63.6166, 65.4682, 69.79784000000001, 72.69049999999999, 75.0, 79.0421,
+           79.9862, 83.88084, 85.58312, 87.71136299999999, 89.0, 91.31840000000001,
+           93.0, 96.05436999999998, 101.1598, 103.0, 106.5111, 107.96322,
+           112.508, 114.9142, 118.8077, 121.85580000000002, 127.69839999999999,
+           127.0, 131.28810000000001, 133.0, 137.42162, 138.99909000000002,
+           140.20986, 141.0, 144.33351666483335, 150.4481, 152.0438, 157.3281,
+           159.0, 162.57152999999997, 165.0, 167.32707000000002, 169.0,
+           173.11618838116186, 175.02820514102572, 178.54163, 180.99988000000002,
+           183.89069999999998, 186.28676, 190.28879711202887, 192.254,
+           195.11347999999998, 197.0, 200.6297, 204.40952, 207.34285, 209.0,
+           232.0, 237.27134]
+el_to_mass = dict(zip(el_norm, mn_norm))
 
 class gamma_tree(object):
     """
@@ -18,7 +51,7 @@ class gamma_tree(object):
     """
     def __init__(self, br_halo_ID, br_age, br_z, br_t_merge, br_ID_merge,
                  br_m_halo, br_r_vir, br_is_prim, redshifts, times,
-                 tree_trunk_ID, ):
+                 tree_trunk_ID, br_is_sub):
         """
         Input and store gamma merger tree info.
         The constructor just takes all the variables needed.
@@ -34,6 +67,7 @@ class gamma_tree(object):
         self.redshifts = redshifts
         self.times = times
         self.tree_trunk_ID = tree_trunk_ID
+        self.br_is_sub = br_is_sub
     
         mfilt = list(filter(None, self.br_m_halo))
         self.Mpeak = np.max(list(itertools.chain.from_iterable(list(itertools.chain.from_iterable(mfilt)))))
@@ -56,22 +90,20 @@ class gamma_tree(object):
         """
         return self.br_halo_ID, self.br_age, self.br_z, self.br_t_merge, \
             self.br_ID_merge, self.br_m_halo, self.br_r_vir, self.br_is_prim, \
-            self.redshifts, self.times, self.tree_trunk_ID
+            self.redshifts, self.times, self.tree_trunk_ID, self.br_is_sub
 
     @property
     def kwargs(self):
         """
         Create kwargs to pass to GAMMA with merger tree data.
-        Does not include 
         """
         kwargs = {"redshifts":self.redshifts, "times":self.times, "br_halo_ID":self.br_halo_ID,
                   "br_age":self.br_age, "br_z":self.br_z, "br_t_merge":self.br_t_merge, 
                   "br_ID_merge":self.br_ID_merge, "br_m_halo":self.br_m_halo,
                   "br_r_vir":self.br_r_vir, "br_is_prim":self.br_is_prim,
                   "tree_trunk_ID":self.tree_trunk_ID,
+                  #"br_is_sub":self.br_is_sub,
                   "m_DM_0":self.m_DM_0}
-        #"br_is_SF":br_is_SF, 
-        #"pre_calculate_SSPs":True,"SSPs_in":SSPs_in, "print_off":True}
         return kwargs
 
     def plot_mass_history(self):
@@ -113,18 +145,180 @@ def generate_kwargs(gt, m_vir_thresh):
                    "SSPs_in":SSPs_in})
     return kwargs
 
-def get_root_Mstar(gam):
+def get_root_Mstar(g):
     """
     """
-def get_root_ZH(gam):
+def get_root_ZH(g):
     """
     """
-def get_root_FeH(gam):
+def get_root_FeH(g):
     """
     """
-def get_root_FeH_distr(gam):
+def get_root_FeH_distr(g):
     """
     """
-def get_root_Mpeak(gam):
+def get_root_Mpeak(g):
     """
     """
+
+def plot_lumfn(Mstars,MoverL=2.0):
+    logLbins = np.arange(1,12,.2)
+    logLs = np.log10(Mstars/MoverL)
+    h,x = np.histogram(logLs, logLbins)
+    
+def mass_to_spectro(M1,M2,e1,e2):
+    """
+    Given two masses M1, M2 and elements e1, e2,
+    return compute [e1/e2]
+    """
+    assert (e1 in el_to_mass) and (e2 in el_to_mass), (e1, e2)
+    assert (e1 in el_to_solar) and (e2 in el_to_solar), (e1, e2)
+    N1 = M1/el_to_mass[e1]
+    N2 = M2/el_to_mass[e2]
+    sol1 = el_to_solar[e1]
+    sol2 = el_to_solar[e2]
+    return np.log10(N1) - np.log10(N2) - sol1 + sol2
+
+### Utility functions for MDF
+def _findBin(x,xRange):
+    ind = 0
+    n = len(xRange)
+    if x > xRange[0]:
+        while ind < n and xRange[ind] < x :
+            ind += 1
+    return ind
+def _areaUnderCurve(y,x,a,b):
+    aIndex = _findBin(a,x)
+    bIndex = _findBin(b,x)
+    return np.trapz(y[aIndex:bIndex+1],x[aIndex:bIndex+1])
+
+def compute_mdf(g, Fe_H_min=-6.0, Fe_H_max=1.0, d_Fe_H=0.05):
+    def get_Fe_H(inst):
+        # Find the Fe index
+        i_Fe = inst.history.elements.index('Fe')
+        # Define the age and Fe_H array
+        age = []
+        Fe_H = []
+        m_locked = []
+        # For each timestep ..
+        for i_t in range(inst.nb_timesteps):
+            # If there are metals ..
+            if inst.ymgal[i_t][2] > 0.0:
+                # Calculate the metallicity 
+                m_Fe_H_ratio = inst.history.ism_elem_yield[i_t][i_Fe] / inst.history.ism_elem_yield[i_t][0]
+                Fe_H.append( np.log10(m_Fe_H_ratio) - np.log10((10**(7.50-12))*56.0) )
+                # Copy the m_locked and age
+                age.append(inst.history.age[i_t])
+                m_locked.append(inst.history.m_locked[i_t])
+        return age, Fe_H, m_locked    
+    
+    nb_Fe_H = int((Fe_H_max - Fe_H_min) / d_Fe_H) + 1
+    mdf_x = np.zeros(nb_Fe_H)
+    mdf_y = np.zeros(nb_Fe_H)
+    for i_Fe in range(nb_Fe_H):
+        mdf_x[i_Fe] = Fe_H_min + i_Fe * d_Fe_H
+    # Create the MDF array of all galaxies
+    mdf_inst = []
+    for i_zz in range(len(g.redshifts)):
+        mdf_inst.append([])
+        for i_br in range(len(g.br_age[i_zz])):
+            mdf_inst[i_zz].append(np.zeros(nb_Fe_H))
+    # Return the [Fe/H] value
+    # For all progenitor galaxies ..
+    for i_zz in range(len(g.redshifts)):
+        for i_br in range(len(g.galaxy_inst[i_zz])):
+    
+            # Verify if the galaxy formed stars ..
+            go_for_it = False
+            if len(g.br_is_SF) == 0:
+                go_for_it = True
+            elif g.br_is_SF[i_zz][i_br]:
+                go_for_it = True
+            if go_for_it:
+                
+                # Copy the galaxy instance
+                inst = g.galaxy_inst[i_zz][i_br].inner
+    
+                # Get the Age - [Fe/H] relation and the stellar mass formed
+                age, Fe_H, m_locked = get_Fe_H(inst)
+    
+                # For each timestep ..
+                for i_t in range(len(age)-1):
+    
+                    if not np.isnan(Fe_H[i_t]) and not np.isnan(Fe_H[i_t+1]) and \
+                       not np.isinf(Fe_H[i_t]) and not np.isinf(Fe_H[i_t+1]) and \
+                        m_locked[i_t] > 0.0:
+    
+                        # Calculate the star formation rate for this timestep
+                        sfr_inst = m_locked[i_t] / (age[i_t+1] - age[i_t])
+                            
+                        # If we do not need to interpolate ..
+                        if Fe_H[i_t+1] == Fe_H[i_t]:
+                            
+                            # Find the lower MDF_x boundary
+                            i_low = 0
+                            while mdf_x[i_low+1] < Fe_H[i_t]:
+                                i_low += 1
+    
+                            # Add the stellar mass in the MDF bin
+                            mdf_inst[i_zz][i_br][i_low] += sfr_inst * (age[i_t+1] - age[i_t])
+    
+                            # Go to the next MDF bin
+                            i_low += 1
+    
+                        # If we need to interpolate
+                        else:
+                            
+                            # Calculate the interpolation coefficients
+                            # t = a * [Fe/H] + b
+                            aa = (age[i_t+1] - age[i_t]) / (Fe_H[i_t+1] - Fe_H[i_t])
+                            bb = age[i_t] - aa * Fe_H[i_t]
+    
+                            # Calculate the maximum and minimum  Fe_H values
+                            # So it doesn't matter when [Fe/H] is increasing or decreasing
+                            min_Fe_inst = min(Fe_H[i_t], Fe_H[i_t+1])
+                            max_Fe_inst = max(Fe_H[i_t], Fe_H[i_t+1])
+    
+                            # Find the lower MDF_x boundary
+                            i_low = 0
+                            while mdf_x[i_low+1] < min_Fe_inst:
+                                i_low += 1
+    
+                            # While some MDF bins still need to be treated ..
+                            while mdf_x[i_low] < max_Fe_inst:
+    
+                                # Get the lower and upper [Fe/H] boundaries covered
+                                # by the OMEGA timesteps within the currend MDF bin
+                                Fe_low = max(mdf_x[i_low], min_Fe_inst)
+                                Fe_up = min(mdf_x[i_low+1], max_Fe_inst)
+    
+                                # Calculate the OMEGA time interval spent on the current MDF bin
+                                t_low = aa * Fe_low + bb
+                                t_up = aa * Fe_up + bb
+                                dt_spent = abs(t_up - t_low)
+    
+                                # Add the stellar mass in the MDF bin
+                                mdf_inst[i_zz][i_br][i_low] += sfr_inst * dt_spent
+    
+                                # Go to the next MDF bin
+                                i_low += 1
+    # Sum all MDFs
+    mdf_all = np.zeros(nb_Fe_H)
+    for i_zz in range(len(g.redshifts)):
+        for i_br in range(len(g.galaxy_inst[i_zz])):
+            mdf_all += mdf_inst[i_zz][i_br]
+    mdf_all_norm = mdf_all/_areaUnderCurve(mdf_all,mdf_x,Fe_H_min,Fe_H_max)
+    
+    return mdf_x, mdf_all_norm
+
+def convolve_mdf(mdf_x, mdf_all, sigma_gauss):
+    Fe_H_min = np.min(mdf_x)
+    d_Fe_H = np.max(np.diff(mdf_x))
+    Fe_H_max = np.max(mdf_x)+d_Fe_H
+    # Convolve with Gaussian
+    mdf_all_gauss = np.zeros(len(mdf_x))
+    for i_x in range(len(mdf_x)):
+        for i_x_scan in range(len(mdf_x)):
+            mdf_all_gauss[i_x_scan] += mdf_all[i_x]*np.exp((-1*(mdf_x[i_x_scan] - mdf_x[i_x])**2) / (2.0*sigma_gauss**2))
+    mdf_all_gauss_norm = mdf_all_gauss/_areaUnderCurve(mdf_all_gauss,mdf_x,Fe_H_min,Fe_H_max)
+    return mdf_all_gauss_norm
--- caga/plot.py
+++ caga/plot.py
@@ -4,7 +4,9 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
-__all__ = ["mass_history"]
+from . import caga
+
+__all__ = ["mass_history","metallicity_evolution"]
 
 def mass_history(gamtree):
     """
@@ -12,7 +14,7 @@ def mass_history(gamtree):
     """
     br_halo_ID, br_age, br_z, br_t_merge, br_ID_merge, \
     br_m_halo, br_r_vir, br_is_prim, redshifts, times, \
-        tree_trunk_ID = [x for x in gamtree.all_vars]
+    tree_trunk_ID, br_is_sub = [x for x in gamtree.all_vars]
     nb_redshifts = len(redshifts)
     
     # Set figure frame and font properties
@@ -37,3 +39,158 @@ def mass_history(gamtree):
     plt.subplots_adjust(left=0.12)
     plt.subplots_adjust(bottom=0.15)
     return fig
+
+def metallicity_evolution(g, tree_trunk_ID=None):
+    """
+    Plot metallicity evolution of each branch
+    Input: g = gamma calculation
+    Optional: tree_trunk_ID if you want to mark a particular branch
+    """
+    # Set figure frame and font properties
+    fig = plt.figure(figsize=(6.5,4.5))
+    matplotlib.rcParams.update({'font.size': 13})
+    
+    # For each star-forming branch ..
+    for i_z in range(len(g.redshifts)):
+        for i_b in range(len(g.br_halo_ID[i_z])):
+            if g.br_is_SF[i_z][i_b]:
+                
+                # Copy the OMEGA (star-forming) calculation
+                o = g.galaxy_inst[i_z][i_b].inner
+                
+                # Check if there is star formation.  Even if this is
+                # technically a star-forming branch, it is possible
+                # that other criteria such as gas density threshold
+                # does not allow star formation.
+                if sum(o.history.m_locked) > 0.0:
+                
+                    # Copy the times and metallicities
+                    t_temp = np.array(o.history.age) + g.times[i_z]
+                    Z_temp = o.history.metallicity
+                
+                    # If the galaxy eventually merges ..
+                    if not g.br_halo_ID[i_z][i_b][-1] == tree_trunk_ID:
+                    
+                        # Copy the timestep at which the galaxy (branch) merged
+                        # Please see the comment below for why the OMEGA+ calculation
+                        # was not simply stopped when the galaxy merged..
+                        i_t_merger = o.i_t_merger
+                        
+                        # Plot the evolution of metallicity from the formation
+                        # of the branch until it merges
+                        plt.plot(t_temp[:i_t_merger+1], Z_temp[:i_t_merger+1])
+    
+                    # Plot the complete evolution if this is the root/trunk
+                    else:
+                        plt.plot(t_temp, Z_temp, linewidth=5, label='Root of the merger tree')
+                    
+    # Set labels
+    plt.yscale('log')
+    plt.xlabel('Time [yr]', fontsize=14)
+    plt.ylabel('Metallicity (mass fraction)', fontsize=14)
+    plt.legend(fontsize=13, frameon=False)
+    plt.ylim(5e-6,2e-2)
+    
+    # Adjust the figure
+    plt.subplots_adjust(top=0.95)
+    plt.subplots_adjust(right=0.95)
+    plt.subplots_adjust(left=0.12)
+    plt.subplots_adjust(bottom=0.15)
+    
+    return fig
+
+def sfh_evolution(g, tree_trunk_ID=None):
+    """
+    Plot sfh evolution of each branch
+    Input: g = gamma calculation
+    Optional: tree_trunk_ID if you want to mark a particular branch
+    """
+    fig = plt.figure(figsize=(6.5,4.5))
+    matplotlib.rcParams.update({'font.size': 13})
+    
+    # For each star-forming branch ..
+    for i_z in range(len(g.redshifts)):
+        for i_b in range(len(g.br_halo_ID[i_z])):
+            if g.br_is_SF[i_z][i_b]:
+                
+                # Copy the OMEGA (star-forming) calculation
+                o = g.galaxy_inst[i_z][i_b].inner
+                
+                # Check if there is star formation
+                if sum(o.history.m_locked) > 0.0:
+                
+                    # Copy the times and star formation rates
+                    t_temp = np.array(o.history.age) + g.times[i_z]
+                    sfh_temp = o.history.sfr_abs
+                
+                    # If the galaxy eventually merges ..
+                    if not g.br_halo_ID[i_z][i_b][-1] == tree_trunk_ID:
+                    
+                        # Plot the evolution of metallicity for from the formation
+                        # of the branch until it merges
+                        i_t_merger = o.i_t_merger
+                        plt.plot(t_temp[:i_t_merger+1], sfh_temp[:i_t_merger+1])
+    
+                    # Plot the complete evolution if this is the root/trunk
+                    else:
+                        plt.plot(t_temp, sfh_temp, linewidth=5, label='Root of the merger tree')
+                    
+    # Set labels
+    plt.yscale('log')
+    plt.xlabel('Time [yr]', fontsize=14)
+    plt.ylabel('Star formation rate [M$_\odot$ yr$^{-1}$]', fontsize=14)
+    plt.legend(fontsize=13, frameon=False)
+    plt.ylim(1e-4,20)
+    
+    # Adjust the figure
+    plt.subplots_adjust(top=0.95)
+    plt.subplots_adjust(right=0.95)
+    plt.subplots_adjust(left=0.12)
+    plt.subplots_adjust(bottom=0.15)
+    
+    return fig
+
+def mstar_evolution(g, reduction_factor=1.0):
+    """
+    Plot total mstar(z)
+    """
+    # saving history of total stellar mass
+    mstar_hist = np.zeros(len(g.redshifts))
+    
+    # For each star-forming branch ..
+    mstar_tot = 0
+    for i_z in range(len(g.redshifts)):
+        for i_b in range(len(g.br_halo_ID[i_z])):
+            if g.br_is_SF[i_z][i_b]:
+                
+                # Copy the OMEGA (star-forming) calculation
+                o = g.galaxy_inst[i_z][i_b].inner
+                
+                mstar_tot += sum(o.history.m_locked)
+        
+        mstar_hist[i_z] = mstar_tot
+    
+    # Set figure frame and font properties
+    fig = plt.figure(figsize=(6,4))
+    matplotlib.rcParams.update({'font.size': 13})
+    
+    plt.plot(g.redshifts,mstar_hist*reduction_factor)
+    plt.yscale('log')
+    plt.xlabel('redshift')
+    plt.ylabel('Total $M_{star}$ [M$_\odot$]')
+    return fig, (g.redshifts, mstar_hist)
+
+def metallicity_distribution(g, sigma_gauss=0.1,
+                             Fe_H_min=-6.0, Fe_H_max=1.0, d_Fe_H=0.05):
+    fig = plt.figure(figsize=(6,4))
+    mdf_x, mdf_all_norm = caga.compute_mdf(g, Fe_H_min=Fe_H_min, Fe_H_max=Fe_H_max, d_Fe_H=d_Fe_H)
+    mdf_all_gauss_norm = caga.convolve_mdf(mdf_x, mdf_all_norm, sigma_gauss)
+    plt.plot(mdf_x,mdf_all_norm, label="Raw MDF")
+    plt.plot(mdf_x,mdf_all_gauss_norm, label="Smoothed MDF")
+    plt.xlim(-3,0.5)
+    #plt.gca().set_yscale('log')
+    matplotlib.rcParams.update({'font.size': 14.0})
+    plt.xlabel('[Fe/H]', fontsize=14)
+    plt.ylabel('MDF', fontsize=14)
+    
+    return fig, (mdf_x, mdf_all_norm, mdf_all_gauss_norm)
